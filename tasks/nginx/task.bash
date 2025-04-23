bae421baab1b09407911162a76531159ad136785
Alexey Melezhik <melezhik@gmail.com>
2025-04-23 17:06:03 +0300

    Update task.bash

--- tasks/nginx/task.bash
+++ tasks/nginx/task.bash
@@ -1,4 +1,5 @@
-set -e
 sudo apk add nginx
 echo "GET 127.0.0.1 test"
-curl http://127.0.0.1 -vs -D - -o /dev/null
+sudo service nginx start 2>&1
+sudo service nginx status
+curl http://127.0.0.1 -s -D - -o /dev/null
